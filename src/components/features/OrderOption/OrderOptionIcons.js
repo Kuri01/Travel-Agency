648ac0e56a53ab71264f3a26fe0d08c0a11f6989
Kuri01 <kamil.kuriata01@gmail.com>
2021-11-27 16:49:06 +0100

    15.3 Fix animation

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -17,19 +17,18 @@ const OrderOptionIcons = ({
         <Icon name={'times-circle'} /> none
       </div>
     )}
-    {values.length
-      ? values.map((value) => (
-          <div
-            className={styles.icon}
-            key={value.id}
-            onClick={() => setOptionValue(value.id)}
-          >
-            {' '}
-            <Icon name={value.icon} />
-            {value.name} ({formatPrice(value.price)})
-          </div>
-        ))
-      : ''}
+    {values.length &&
+      values.map((value) => (
+        <div
+          className={styles.icon}
+          key={value.id}
+          onClick={() => setOptionValue(value.id)}
+        >
+          {' '}
+          <Icon name={value.icon} />
+          {value.name} ({formatPrice(value.price)})
+        </div>
+      ))}
   </div>
 );
 export default OrderOptionIcons;
